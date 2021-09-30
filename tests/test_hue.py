bee1543bea59722a02b57057fa156c7dd764e5c8
Nick Donaldson <ndonald2@gmail.com>
2021-09-30 14:09:15 -0600

    Add multi-light message test

--- tests/test_hue.py
+++ tests/test_hue.py
@@ -22,7 +22,14 @@ class TestHueStreamMessage:
         assert message.bytes.startswith(header)
 
     def test_message_single_light(self, message):
-        rgb = (9865, 2048, 29398)
-        message.add(5, rgb)
+        message.add(5, (9865, 2048, 29398))
         assert message.bytes[-9:-6] == b'\x00\x00\x05'
         assert message.bytes[-6:] == b'\x26\x89\x08\x00\x72\xd6'
+
+    def test_message_multi_lights(self, message):
+        message.add(23, (9993, 2048, 29398))
+        message.add(9, (16, 255, 16385))
+        assert message.bytes[-18:-15] == b'\x00\x00\x17'
+        assert message.bytes[-15:-9] == b'\x27\x09\x08\x00\x72\xd6'
+        assert message.bytes[-9:-6] == b'\x00\x00\x09'
+        assert message.bytes[-6:] == b'\x00\x10\x00\xff\x40\x01'
