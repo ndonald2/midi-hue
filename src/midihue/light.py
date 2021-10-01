0cb1a76dceb44b24b7b723e18e76e0cb52290ef3
Nick Donaldson <ndonald2@gmail.com>
2021-09-30 21:05:23 -0600

    Add hsv utils

--- src/midihue/light.py
+++ src/midihue/light.py
@@ -1,8 +1,15 @@
+from collections import namedtuple
 from colorsys import rgb_to_hsv, hsv_to_rgb
 
 
 def _clampunit(i):
     return max(0., min(1.0, i))
+
+
+RGB = namedtuple('RGB', ['r', 'g', 'b'])
+
+
+HSV = namedtuple('HSV', ['h', 's', 'v'])
 
 
 class Light:
@@ -22,17 +29,70 @@ class Light:
 
     def __setattr__(self, name, value):
         if name in ('r', 'g', 'b'):
+            # Clamp to 0.0 - 1.0 float range
             setattr(self, f'_{name}', _clampunit(value))
         else:
             object.__setattr__(self, name, value)
 
+    @property
     def rgb(self):
-        return (self.r, self.g, self.b)
+        return RGB(self.r, self.g, self.b)
 
+    @rgb.setter
+    def rgb(self, value):
+        assert len(value) == 3
+        self.r = value[0]
+        self.g = value[1]
+        self.b = value[2]
+
+    @property
     def rgb_int(self):
         max_int = (1 << self._bits_per_channel) - 1
         return (
             int(self.r * max_int),
             int(self.g * max_int),
             int(self.b * max_int)
+        )
+
+    @property
+    def h(self):
+        return self.hsv.h
+
+    @h.setter
+    def h(self, value):
+        hsv = self.hsv
+        hsv.h = _clampunit(value)
+        self.rgb = hsv_to_rgb(*hsv)
+
+    @property
+    def s(self):
+        return self.hsv.s
+
+    @s.setter
+    def s(self, value):
+        hsv = self.hsv
+        hsv.s = _clampunit(value)
+        self.rgb = hsv_to_rgb(*hsv)
+
+    @property
+    def v(self):
+        return self.hsv.v
+
+    @v.setter
+    def v(self, value):
+        hsv = self.hsv
+        hsv.v = _clampunit(value)
+        self.rgb = hsv_to_rgb(*hsv)
+
+    @property
+    def hsv(self):
+        return HSV(*rgb_to_hsv(*self.rgb))
+
+    @hsv.setter
+    def hsv(self, value):
+        assert len(value) == 3
+        self.rgb = rgb_to_hsv(
+            _clampunit(value[0]),
+            _clampunit(value[1]),
+            _clampunit(value[2])
         )
--- tests/test_light.py
+++ tests/test_light.py
@@ -7,28 +7,63 @@ class TestLight:
     @pytest.fixture
     def light(self):
         light = Light(7)
-        light.r = 0.2
+        light.r = 0.15
         light.g = 0.3
         light.b = 0.4
         return light
 
-    def test_light_id(self, light):
-        assert light.light_id == 7
-
-    def test_get_set_r(self, light):
-        light.r = 0.7
-        assert light.r == 0.7
-
-    def test_get_set_g(self, light):
-        light.g = 0.7
-        assert light.g == 0.7
-
-    def test_get_set_b(self, light):
-        light.b = 0.7
-        assert light.b == 0.7
+    # --- RGB ---
 
     def test_get_rgb(self, light):
-        assert light.rgb() == (0.2, 0.3, 0.4)
+        assert light.rgb == (0.15, 0.3, 0.4)
+
+    @pytest.mark.parametrize(
+        'value,expected',
+        [
+            ((0.1, 0.75, 0.22), (0.1, 0.75, 0.22)),
+            ((-0.1, 5, -4), (0.0, 1.0, 0.0))
+        ]
+    )
+    def test_set_rgb(self, light, value, expected):
+        light.rgb = value
+        assert light.rgb == expected
+        assert light.r == expected[0]
+        assert light.g == expected[1]
+        assert light.b == expected[2]
 
     def test_get_rgb_int(self, light):
-        assert light.rgb_int() == (13107, 19660, 26214)
+        assert light.rgb_int == (9830, 19660, 26214)
+
+    @pytest.mark.parametrize(
+        'value,expected',
+        [(0.7, 0.7), (-0.2, 0.0), (30, 1.0)]
+    )
+    class TestRGBSetters:
+
+        def test_get_set_r(self, light, value, expected):
+            light.r = value
+            assert light.r == expected
+
+        def test_get_set_g(self, light, value, expected):
+            light.g = value
+            assert light.g == expected
+
+        def test_get_set_b(self, light, value, expected):
+            light.b = value
+            assert light.b == expected
+
+    # --- HSV ---
+
+    def test_get_hsv(self, light):
+        assert light.hsv == (0.5666666666666668, 0.625, 0.4)
+
+    @pytest.mark.parametrize(
+        'value,expected_rgb',
+        [
+            ((0.5, 1.0, 1.0), (0.5, 0.5, 1.0)),
+            ((-0.1, 5, -4), (1.0/3.0, 1.0, 1.0))
+        ]
+    )
+    def test_set_hsv(self, light, value, expected_rgb):
+        light.hsv = value
+        assert light.rgb == expected_rgb
